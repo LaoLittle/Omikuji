f8d585082cde236911bf7f8bada0bf0a7f1df903
LaoLittle <y1941803875@outlook.com>
2021-12-19 19:36:04 +0800

    update

--- src/main/kotlin/Omikuji.kt
+++ src/main/kotlin/Omikuji.kt
@@ -26,18 +26,17 @@ object Omikuji : KotlinPlugin(
         GlobalEventChannel.subscribeMessages {
             finding(Regex("求签")) {
                 val date = LocalDate.now()
-                val accdate = "${date.year}${date.monthValue}${date.dayOfMonth}".toLong()
-                val random = Random(sender.id + accdate).nextInt(0, 40)
-                omkj = omikujiInfo[random]
-                userrandom = userrandom.plus(Pair(sender.id, accdate))
+                val accDate = "${date.year}${date.monthValue}${date.dayOfMonth}".toLong()
+                val random = Random(sender.id + accDate).nextInt(0, 60)
+                val omkj = omikujiInfo[random] ?: throw NoSuchElementException("未找到对应签！")
                 val forwardMessage = buildForwardMessage(
-                    displayStrategy = CustomForwardMsgDisplay
+                    displayStrategy = CustomForwardMsgDisplay(omkj)
                 ) {
-                    add(sender, PlainText(omkj?.first.toString()))
-                    add(sender, PlainText(omkj?.second.toString()))
-                    add(sender, PlainText(omkj?.third.toString()))
-                    add(sender, PlainText(omkj?.fourth.toString()))
-                    add(sender, PlainText(omkj?.attr.toString()))
+                    add(sender, PlainText(omkj.first))
+                    add(sender, PlainText(omkj.second))
+                    add(sender, PlainText(omkj.third))
+                    add(sender, PlainText(omkj.fourth))
+                    add(sender, PlainText(omkj.attr))
                 }
                 subject.sendMessage(forwardMessage)
             }
